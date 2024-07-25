c614fe764ce914b352f39b8061e4dc104a7a7331
moon <y222t179@kyoto-seika.ac.jp>
2024-07-25 17:25:30 +0900

    japanese font

--- src/app.js
+++ src/app.js
@@ -1,57 +1,32 @@
-async function generatePDF() {
-    const { PDFDocument, rgb } = PDFLib;
+import jsPDF from 'jspdf';
+import { font } from "./NotoSansJP-normal"
+const FONT_URL = 'font/NotoSansJP-VariableFont_wght.ttf';
 
-    // フォームのデータを取得
+
+document.getElementById('generate-pdf').addEventListener('click', () => {
+
     const technology = document.getElementById('technology').value;
-    const experience = document.getElementById('experience').value;
-    const pr = document.getElementById('pr').value;
 
-    // 新しいPDFドキュメントを作成
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage([600, 400]);
-    const { width, height } = page.getSize();
-    const fontSize = 30;
+    // 新規インスタンス作成
+    const doc = new jsPDF();
 
-    // フォントの読み込み
-    const fontUrl = './font/NotoSansJP-VariableFont_wght.ttf';
-    const fontBytes = await fetch(fontUrl).then(res => res.arrayBuffer());
-    const customFont = await pdfDoc.embedFont(fontBytes);
+    // フォント指定
+    doc.setTextColor(0, 0, 0)
+    doc.addFileToVFS(FONT_URL, font);
+    doc.addFont(FONT_URL, 'NotoSansJP-Regular', 'normal');
 
-    // テキストを追加
-    page.drawText('あなたのスキルシート', {
-        x: 50,
-        y: height - 4 * fontSize,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(`得意な技術: ${technology}`, {
-        x: 50,
-        y: height - 6 * fontSize,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(`経歴: ${experience}`, {
-        x: 50,
-        y: height - 8 * fontSize,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-    });
-    page.drawText(`自己PR: ${pr}`, {
-        x: 50,
-        y: height - 10 * fontSize,
-        size: fontSize,
-        font: customFont,
-        color: rgb(0, 0, 0),
-    });
+    // フォントを設定
+    doc.setFont('NotoSansJP-Regular');
 
-    // PDFをダウンロード
-    const pdfBytes = await pdfDoc.save();
-    const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'SkillSheet.pdf';
-    link.click();
-}
+
+    // テキスト追加
+    doc.text(`得意な技術: ${technology}`, 20, 20);
+    doc.text('Hello world!', 10, 10);
+
+    // // 画像追加
+    // const logoImg = '<path to image>'; 
+    // doc.addImage(logoImg, 'JPEG', 10, 20, 40, 40); // 画像の配置位置とサイズを指定
+
+    // ファイルの保存（オプション）
+    doc.save('example.pdf');
+});
