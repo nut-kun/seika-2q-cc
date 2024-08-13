f70e51b0bcfd86363495e37a949ebef01b6ddd52
moon <y222t179@kyoto-seika.ac.jp>
2024-08-13 11:40:52 +0900

    refactor 質問文追加

--- src/app.js
+++ src/app.js
@@ -5,28 +5,22 @@ const FONT_URL = 'font/NotoSansJP-VariableFont_wght.ttf';
 
 document.getElementById('generate-pdf').addEventListener('click', () => {
 
-    const technology = document.getElementById('technology').value;
+    const no = document.getElementById('no').value;
+    const name = document.getElementById('name').value;
 
-    // 新規インスタンス作成
     const doc = new jsPDF();
 
-    // フォント指定
     doc.setTextColor(0, 0, 0)
     doc.addFileToVFS(FONT_URL, font);
     doc.addFont(FONT_URL, 'NotoSansJP-Regular', 'normal');
 
-    // フォントを設定
     doc.setFont('NotoSansJP-Regular');
 
 
-    // テキスト追加
-    doc.text(`得意な技術: ${technology}`, 20, 20);
-    doc.text('Hello world!', 10, 10);
+    doc.text('京都精華大学', 10, 10);
+    doc.text(`学籍番号: ${no}`, 10, 20);
+    doc.text(`名前: ${name}`, 10, 30);
 
-    // // 画像追加
-    // const logoImg = '<path to image>'; 
-    // doc.addImage(logoImg, 'JPEG', 10, 20, 40, 40); // 画像の配置位置とサイズを指定
 
-    // ファイルの保存（オプション）
     doc.save('example.pdf');
 });
